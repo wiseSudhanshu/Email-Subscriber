93989e9b8e7918e760b214de67719ce1978ac3d0
Sudhanshu Rai <sudhanshu.rai@wisdmlabs.com>
2023-03-31 18:44:55 +0530

    added the plugin boilerplate

---
+++ includes/class-email-subscriber-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://sudhanshu.wisdmlabs.net/
+ * @since      1.0.0
+ *
+ * @package    Email_Subscriber
+ * @subpackage Email_Subscriber/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Email_Subscriber
+ * @subpackage Email_Subscriber/includes
+ * @author     Sudhanshu Rai <sudhanshu.rai@wisdmlabs.com>
+ */
+class Email_Subscriber_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-email-subscriber-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://https://sudhanshu.wisdmlabs.net/
+ * @since      1.0.0
+ *
+ * @package    Email_Subscriber
+ * @subpackage Email_Subscriber/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Email_Subscriber
+ * @subpackage Email_Subscriber/includes
+ * @author     Sudhanshu Rai <sudhanshu.rai@wisdmlabs.com>
+ */
+class Email_Subscriber_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
